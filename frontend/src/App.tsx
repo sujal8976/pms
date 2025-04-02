99bf611b3f77eb3c557a9402cfb188b4f995324a
Sujal Gupta <sujalgupta8976@gmail.com>
2025-04-02 14:46:23 +0530

    frontend bootstrapped and added tailwind, shadcn

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+
+  return (
+    <>
+      <div className="text-9xl bg-blue-700">Hello World</div>
+      <Button className="p-10">Click</Button>
+    </>
+  );
+}
+
+export default App;
